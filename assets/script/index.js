1e631d498588122acefee2ec1ed8bca21d448b84
Temi-Tade <akintadetemi19@gmail.com>
2024-08-10 19:32:20 +0100

    added english voices

--- assets/script/index.js
+++ assets/script/index.js
@@ -4,8 +4,15 @@ const API_KEY = "AIzaSyCChmxrMMiTncdIOt2QpsRKYnzKvL1Bw4k";
 const genAI = new GoogleGenerativeAI(API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 const voices = [];
-speechSynthesis.onvoiceschanged = (e) => {
-    voices.push(...speechSynthesis.getVoices());
+const voiceoptions = [];
+let voice;
+
+speechSynthesis.onvoiceschanged = () => {
+    [...speechSynthesis.getVoices()].filter(voice => voice.name.toLowerCase().includes("english")).map(voice  => {
+        voices.push(voice);
+        voiceoptions.push(`<option>${voice.name}</option>`);
+    });
+    voice = voices[0]
 }
 
 const SHOW_MODAL = (text) => {
@@ -50,6 +57,8 @@ const SHOW_MODAL = (text) => {
 }
 
 const CHECK_MIC_PERMISSION = () => {
+    navigator.mediaDevices.getUserMedia({audio: true})
+    .then(res => console.log(res))
     return navigator.permissions.query({name: "microphone"});
 }
 
@@ -59,63 +68,66 @@ document.querySelector(".speak-btn-wrap button").onclick = (ev) => {
             if ('webkitSpeechRecognition' in window) {
                 try {
                     ev.target.setAttribute("class", "fa-solid fa-microphone-lines fa-shake");
-                var recognition = new webkitSpeechRecognition();
-                let transcript = "";
-                let prompt = "";
-                recognition.continuous = false;
-                recognition.interimResults = false;
-                recognition.lang = "en-US";
-            
-                recognition.onstart = () => {
-                    console.log('Speech recognition started');
-                };
-            
-                recognition.onerror = async (event) => {
-                    let error = await event.error;
-                    alert('Speech recognition error: An error occured', error);
-                };
+                    document.querySelector(".info").innerHTML = "Speak now..."
+                    var recognition = new webkitSpeechRecognition();
+                    let transcript = "";
+                    let prompt = "";
+                    recognition.continuous = false;
+                    recognition.interimResults = false;
+                    recognition.lang = "en-US";
+                
+                    recognition.onstart = () => {
+                        console.log('Speech recognition started');
+                    };
+                
+                    recognition.onerror = async (event) => {
+                        let error = await event.error;
+                        document.querySelector(".info").innerHTML = "Click on the microphone and speak";
+                        SHOW_MODAL('Speech recognition error: An error occured', error);
+                    };
 
-                recognition.onnomatch = () => {
-                    alert("Please try again. I did not catch that.")
-                };
-            
-                recognition.onend = () => {
-                    console.log('Speech recognition ended');
-                    ev.target.setAttribute("class", "fa-solid fa-microphone-lines"); 
-                };
-            
-                recognition.onresult = async (event) => {
-                    transcript = event.results[0][0].transcript;
-                    prompt = `${transcript}`;
-                    console.log(prompt);
-                    const result = await model.generateContent(prompt);
-                    const response = await result.response;
-                    const text = response.text();
-                    var speak = new SpeechSynthesisUtterance(text.replaceAll("*", "").replaceAll("#", ""));
-        
-                    speak.lang = "en-US";
-                    speak.voice = voices[2];
-                    speechSynthesis.speak(speak);
-                    speak.onstart = () => {
-                        ev.target.setAttribute("class", "fa-solid fa-bullseye fa-beat");
-                    }
-        
-                    speak.onend = () => {
+                    recognition.onnomatch = () => {
+                        SHOW_MODAL("Please try again. I did not catch that.")
+                    };
+                
+                    recognition.onend = () => {
+                        console.log('Speech recognition ended');
                         ev.target.setAttribute("class", "fa-solid fa-microphone-lines");
-                    }
-                    
-                    console.log(text);
-                };
+                    };
+                
+                    recognition.onresult = async (event) => {
+                        transcript = event.results[0][0].transcript;
+                        prompt = `${transcript}`;
+                        console.log(prompt);
+                        const result = await model.generateContent(prompt);
+                        const response = await result.response;
+                        const text = response.text();
+                        var speak = new SpeechSynthesisUtterance(text.replaceAll("*", "").replaceAll("#", ""));
             
-                recognition.start();
+                        speak.lang = "en-US";
+                        speak.voice = voice;
+                        speechSynthesis.speak(speak);
+                        speak.onstart = () => {
+                            ev.target.setAttribute("class", "fa-solid fa-bullseye fa-beat");
+                        }
+            
+                        speak.onend = () => {
+                            ev.target.setAttribute("class", "fa-solid fa-microphone-lines");
+                            document.querySelector(".info").innerHTML = "Click on the microphone and speak";
+                        }
+                        
+                        console.log(text);
+                    };
+                
+                    recognition.start();
                 } catch (error) {
                     console.error(error);
                 }
             } else {
-                alert('Speech recognition not supported in this browser.');
+                SHOW_MODAL('Speech recognition not supported in this browser.');
             }
         } else {
-            alert("Allow this site to access microphone in your site settings.")
+            SHOW_MODAL("Allow this site to access microphone in your site settings.")
         }
     })
 }
@@ -123,6 +135,17 @@ document.querySelector(".speak-btn-wrap button").onclick = (ev) => {
 document.querySelector(".settings-btn").onclick = () => {
     SHOW_MODAL(`
         <h3>Settings</h3>
-
+        <ul class='settings-list' type="none">
+            <li>
+                <span>AI voice</span>
+                <p>
+                    <select>${voiceoptions}</select>
+                </p>
+            </li>
+        </ul>
     `)
+
+    document.querySelector("select").onchange = (ev) => {
+        voice = voices[voiceoptions.indexOf(`<option>${ev.target.value}</option>`)];
+    }
 }
